63f9248d948b0b353aa579ce080b008f281285f9
kawabata <kawabata@eu>
2015-01-09 11:35:01 +0900

    Created Context Munu on TexitView

--- jmatx-android-graph/src/main/java/org/mklab/matx/android/graph/MainActivity.java
+++ jmatx-android-graph/src/main/java/org/mklab/matx/android/graph/MainActivity.java
@@ -51,6 +51,8 @@ import android.text.SpannedString;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.ActionMode;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Display;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -62,6 +64,7 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -121,6 +124,8 @@ public class MainActivity extends Activity implements KeyboardListner,
 	private List<String> commandList = new ArrayList<>();
 	private List<String> methodNameList = new ArrayList<>();
 	private int prefontsize = 15;
+	protected int mTouchX;
+	protected int mTouchY;
 
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
@@ -151,6 +156,42 @@ public class MainActivity extends Activity implements KeyboardListner,
 		}
 	}
 
+	static final int CONTEXT_MENU1_ID = 0;
+	static final int CONTEXT_MENU2_ID = 1;
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+
+		super.onCreateContextMenu(menu, v, menuInfo);
+
+		// コンテキストメニューの設定
+		menu.setHeaderTitle("メニュータイトル");
+		// Menu.add(int groupId, int itemId, int order, CharSequence title)
+		menu.add(0, CONTEXT_MENU1_ID, 0, "メニュー1");
+		menu.add(0, CONTEXT_MENU2_ID, 0, "メニュー2");
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+
+		switch (item.getItemId()) {
+		case CONTEXT_MENU1_ID:
+			// TODO:メニュー押下時の操作
+			mTextView.setSelected(true);		
+		
+			return true;
+		case CONTEXT_MENU2_ID:
+			// TODO:メニュー押下時の操作
+			return true;
+		default:
+			return super.onContextItemSelected(item);
+		}
+	}
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -161,7 +202,15 @@ public class MainActivity extends Activity implements KeyboardListner,
 
 		this.mTextView = (TextView) findViewById(R.id.termWindow);
 		this.mTextView.setTextIsSelectable(true);
-
+		registerForContextMenu(this.mTextView);
+//		mTextView.setOnTouchListener(new View.OnTouchListener() {
+//			@Override
+//			public boolean onTouch(View v, MotionEvent event) {
+//				mTouchX = (int) event.getX();
+//				mTouchY = (int) event.getY();
+//				return false;
+//			}
+//		});
 		this.mTextView
 				.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
 
@@ -246,8 +295,11 @@ public class MainActivity extends Activity implements KeyboardListner,
 												+ beforeLineCount].split(" ");
 										System.out.println("S.O! Crue!! "
 												+ bitmapMarks[1]);
-										createFolderSaveImage(		MainActivity.this.bitmaps.get(Integer
-												.valueOf(bitmapMarks[1])),"TESTTEST" + bitmapMarks[1]);										
+										createFolderSaveImage(
+												MainActivity.this.bitmaps
+														.get(Integer
+																.valueOf(bitmapMarks[1])),
+												"TESTTEST" + bitmapMarks[1]);
 									}
 									System.out
 											.println(i
