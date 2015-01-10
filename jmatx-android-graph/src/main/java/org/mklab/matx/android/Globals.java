5c5fb925ddc9bec5234be27999736d2141918091
Created Globals.java to kawabata <kawabata@eu>
2015-01-11 00:06:28 +0900

    Created Globals to share variable

---
+++ jmatx-android-graph/src/main/java/org/mklab/matx/android/Globals.java
@@ -0,0 +1,11 @@
+package org.mklab.matx.android;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Application;
+import android.graphics.Bitmap;
+
+public class Globals extends Application {
+	List<Bitmap> bitmaps = new ArrayList<>();
+}
